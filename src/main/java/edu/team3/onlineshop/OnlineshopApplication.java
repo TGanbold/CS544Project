e59429a1688c24f6bcc94ce8a11a526baab36708
Tsogzolmaa <46358458+TGanbold@users.noreply.github.com>
2020-04-07 19:29:08 -0600

    Project first upload

---
+++ src/main/java/edu/team3/onlineshop/OnlineshopApplication.java
@@ -0,0 +1,13 @@
+package edu.team3.onlineshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineshopApplication.class, args);
+	}
+
+}
